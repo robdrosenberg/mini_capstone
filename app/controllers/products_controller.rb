0500af17b59ac2c3fdff0677899b311bee4a5c2e
robdrosenberg <robertdr85@gmail.com>
2018-01-16 20:08:14 -0800

    Added json pages for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,14 @@ class ProductsController < ApplicationController
 
     render json: products.as_json
   end
+
+  def blue_yeti
+    product = Product.find_by(title: "BlueYeti Microphone")
+    render json: product.as_json
+  end
+
+  def seiko_watch
+    product = Product.find_by(title: "Seiko Watch")
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/all_products" => "products#all_products"
+  get "/blue_yeti" => "products#blue_yeti"
+  get "/seiko_watch" => "products#seiko_watch"
 
 end
