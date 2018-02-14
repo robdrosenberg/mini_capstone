0fe6d394f4edf8532622bf32b555421b0f0f4123
Robert Rosenberg <robertdr85@gmail.com>
2018-02-13 18:55:05 -0800

    Added template cards

---
+++ public/index.js
@@ -0,0 +1,6 @@
+var productTemplate = document.querySelector("#product-card");
+var productRow = document.querySelector(".row");
+
+productRow.appendChild(productTemplate.content.cloneNode(true));
+productRow.appendChild(productTemplate.content.cloneNode(true));
+productRow.appendChild(productTemplate.content.cloneNode(true));
